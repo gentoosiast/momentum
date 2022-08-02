7cb1357921363597e559564027948dc022212988
Sergey Koksharov <gentoosiastic@gmail.com>
2022-08-02 14:31:25 +0300

    refactor: use date provided as argument in functions of datetime module

--- momentum/src/js/datetime.js
+++ momentum/src/js/datetime.js
@@ -1,21 +1,18 @@
 export default {
-  getTime(locale = 'en-US') {
-    const date = new Date();
+  getTime(date, locale = 'en-US') {
     // https://stackoverflow.com/questions/68646411/date-tolocalestringen-us-hour12-false-is-providing-midnight-as-24
     const options = { hourCycle: 'h23' }; // task requirements
 
     return date.toLocaleTimeString(locale, options);
   },
 
-  getDate(locale = 'en-US') {
-    const date = new Date();
+  getDate(date, locale = 'en-US') {
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
 
     return date.toLocaleDateString(locale, options);
   },
 
-  getTimeOfDay() {
-    const date = new Date();
+  getTimeOfDay(date) {
     const hours = date.getHours();
     if (hours >= 6 && hours <= 11) {
       return 'morning';
