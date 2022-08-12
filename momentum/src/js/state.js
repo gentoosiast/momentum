f4d844bf813611870711f95d34c4dbf5bed392f1
Sergey Koksharov <gentoosiastic@gmail.com>
2022-08-12 22:12:17 +0300

    refactor: redesign state module which provides default settings and saving/loading from localStorage

--- momentum/src/js/state.js
+++ momentum/src/js/state.js
@@ -1,11 +1,28 @@
+const defaultSettings = {
+  userName: '',
+  locale: 'ru-RU',
+  imageProvider: 'github',
+  imageTags: [],
+  city: 'Minsk',
+  weatherLastRequest: 0,
+  tasks: [],
+  widgets: [
+    'clock',
+    'calendar',
+    'greeting',
+    'quote',
+    'weather',
+    'audio',
+    'tasklist',
+  ],
+};
+
 export default {
-  load() {
-    return {
-      locale: localStorage.getItem('locale') ?? 'en-US',
-      userName: localStorage.getItem('userName'),
-      city: localStorage.getItem('city') ?? 'Minsk',
-      // 0 if not set
-      weatherLastRequest: Number(localStorage.getItem('weatherLastRequest')),
-    };
+  get(key) {
+    return JSON.parse(localStorage.getItem(key)) ?? defaultSettings[key];
+  },
+
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
   },
 };
