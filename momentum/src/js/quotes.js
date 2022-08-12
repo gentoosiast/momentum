3f5cb04ac6eed8735eb8167c7b54f5e99ed847fb
Sergey Koksharov <gentoosiastic@gmail.com>
2022-08-12 22:06:59 +0300

    refactor: rewrite quote widget and extract its code from index.js to standalone module

--- momentum/src/js/quotes.js
+++ momentum/src/js/quotes.js
@@ -1,25 +1,45 @@
+import state from './state';
 import utils from './utils';
 
+let quotesData = [];
+let lastQuoteIdx = -1;
+
+const quoteText = document.querySelector('.quote-widget__text');
+const quoteAuthor = document.querySelector('.quote-widget__author');
+const quoteButton = document.querySelector('.quote-widget__reload');
+
+async function fetchQuotes(lang = 'en') {
+  const quotes = `./assets/quotes-${lang}.json`;
+  const res = await fetch(quotes);
+  const data = await res.json();
+
+  return data;
+}
+
+function update() {
+  let idx;
+  do {
+    idx = utils.getRandomNum(0, quotesData.length - 1);
+  } while (idx === lastQuoteIdx);
+  lastQuoteIdx = idx;
+  const quote = quotesData[idx];
+  quoteText.textContent = quote.text;
+  quoteAuthor.textContent = quote.author;
+}
+
+quoteButton.addEventListener('click', update);
+
 export default {
-  quotesData: [],
-  lastQuoteIdx: -1,
-
-  getRandomQuote() {
-    let idx;
-    do {
-      idx = utils.getRandomNum(0, this.quotesData.length - 1);
-    } while (idx === this.lastQuoteIdx);
-    this.lastQuoteIdx = idx;
-    return this.quotesData[idx];
-  },
-
-  async loadQuotes(lang = 'en') {
-    const quotes = `./assets/quotes-${lang}.json`;
-    const res = await fetch(quotes);
-    const data = await res.json();
-
-    this.lastQuoteIdx = -1;
-    this.quotesData = data;
-    return data;
+  update,
+  init() {
+    const lang = utils.getCountryCode(state.get('locale'));
+    fetchQuotes(lang)
+      .then((data) => {
+        quotesData = data;
+        this.update();
+      })
+      .catch((e) => {
+        utils.showError(`quotes widget: ${e}`);
+      });
   },
 };
