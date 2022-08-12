37398b982984fd90850d27b9b6729a2c790235b8
Sergey Koksharov <gentoosiastic@gmail.com>
2022-08-12 21:54:49 +0300

    refactor: provide Date object as default parameter in functions of datetime module

--- momentum/src/js/datetime.js
+++ momentum/src/js/datetime.js
@@ -1,18 +1,18 @@
 export default {
-  getTime(date, locale = 'en-US') {
+  getTime(date = new Date(), locale = 'en-US') {
     // https://stackoverflow.com/questions/68646411/date-tolocalestringen-us-hour12-false-is-providing-midnight-as-24
     const options = { hourCycle: 'h23' }; // task requirements
 
     return date.toLocaleTimeString(locale, options);
   },
 
-  getDate(date, locale = 'en-US') {
+  getDate(date = new Date(), locale = 'en-US') {
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
 
     return date.toLocaleDateString(locale, options);
   },
 
-  getTimeOfDay(date) {
+  getTimeOfDay(date = new Date()) {
     const hours = date.getHours();
     if (hours >= 6 && hours <= 11) {
       return 'morning';
